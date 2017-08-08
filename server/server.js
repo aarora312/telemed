bb6b22a673522041b6336006f746c8847e97a604
Anvi Arora <anvi.arora@matrixcare.com>
2017-08-08 15:32:10 -0500

    Added root to res.sendFile path

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const serverConfig = {
 // ----------------------------------------------------------------------------------------
 
 app.get('/', function (req, res) {
-    res.sendFile('/client/index.html');
+    res.sendFile('client/index.html', { root: '/c/Users/aarora/Documents/heroku-telemed' });
 })
 // Create a server for the client html page
 /*var handleRequest = function(request, response) {
